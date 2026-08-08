b592018df276dd8f0b0a2313af3d60e7b7d4c13a
Meen Budha <141411070+Meenbudha@users.noreply.github.com>
2026-08-08 20:40:45 +0530

    Time: 1 ms (42.5%), Space: 46.4 MB (70.83%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        solve(ans, root);
+        return ans;
+    }
+
+    private void solve(List<Integer> ls, Node root){
+        if(root == null) return;
+        ls.add(root.val);
+
+        for(Node child : root.children){
+            solve(ls, child);
+        }
+    }
+}
